a031829d267be2d9ba175e88801f14f9d434e634
jeonhaelee <jhealee0624@gmail.com>
2022-03-20 23:36:16 +0900

    올바른 괄호 1 클린 코드

--- codeup_1410.py
+++ codeup_1410.py
@@ -1,10 +1,12 @@
 arr = input()
 
-count_1 = 0; count_2 = 0
-for i in range(len(arr)):
-    if arr[i] == "(":
-        count_1 += 1
-    elif arr[i] == ")":
-        count_2 += 1
+# count_1 = 0; count_2 = 0
+# for i in range(len(arr)):
+#     if arr[i] == "(":
+#         count_1 += 1
+#     elif arr[i] == ")":
+#         count_2 += 1
         
-print("{} {}".format(count_1, count_2))
+# print("{} {}".format(count_1, count_2))
+
+print("{} {}".format(arr.count("("),arr.count(")")))
