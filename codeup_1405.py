b4f9cc75e9a275961bc02d4784fadd4f08c231d2
jeonhaelee <jhealee0624@gmail.com>
2022-03-20 23:23:21 +0900

    숫자 로테이션

--- codeup_1405.py
+++ codeup_1405.py
@@ -1,16 +1,18 @@
 n = int(input())
 
-d = []
-d.append(input().split())
+d = input().split()
 
 for i in range(n):
     print(d[i], end = " ")
+    
+print()
 
-for j in range(n):
-    for i in range(n):
-        print(d[i], end = " ")
+for j in range(n-1):
     for i in range(n-1):
         temp = d[i]
         d[i] = d[i+1]
         d[i+1] = temp
+    for i in range(n):
+        print(d[i], end = " ")
+    print()
     
