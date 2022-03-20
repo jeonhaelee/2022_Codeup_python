614dc5cba059948f6a1cafcaba6399df3a3521f6
jeonhaelee <jhealee0624@gmail.com>
2022-03-20 23:33:04 +0900

    올바른 괄호 1

---
+++ codeup_1410.py
@@ -0,0 +1,10 @@
+arr = input()
+
+count_1 = 0; count_2 = 0
+for i in range(len(arr)):
+    if arr[i] == "(":
+        count_1 += 1
+    elif arr[i] == ")":
+        count_2 += 1
+        
+print("{} {}".format(count_1, count_2))
