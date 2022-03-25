180f3a3838adeef7cf370627f68dde02a2bbf92a
jeonhaelee <jhealee0624@gmail.com>
2022-03-25 15:21:27 +0900

    2차원 배열 순서대로 채우기 1-7

--- codeup_1466.py
+++ codeup_1466.py
@@ -1,15 +1,7 @@
-import numpy as np
-
 n, m = map(int, input().split())
-arr = np.arange(n*m)
-arr.shape = (n,m)
-arr.tolist()
 
 for i in range(n):
     for j in range(m):
-        arr[i][j] = (n * m - i) - n * j
-        
-for i in range(n):
-    for j in range(m):
-        print(arr[i][j], end = " ")
+        print((n * m - i) - n * j, end = " ")
     print()
+        
