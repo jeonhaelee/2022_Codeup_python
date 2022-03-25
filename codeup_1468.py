1e76e0bfc7b06574c51f15ccec1c9d4ab015477f
jeonhaelee <jhealee0624@gmail.com>
2022-03-25 15:44:44 +0900

    2차원 배열 지그재그 채우기 2-1

--- codeup_1468.py
+++ codeup_1468.py
@@ -1,15 +1,10 @@
 
-import numpy as np
-
 n = int(input())
-
-d = np.arange(n*n)
-d.shape = (n,n)
 
 for i in range(n):
     for j in range(n):
         if i % 2 == 0:
-            print(d[i,j]+1, end = " ")
+            print(i * n + 1 + j, end = " ")
         else :
-            print(d[i,n-1-j]+1, end = " ")
+            print(n * (i + 1) - j, end = " ")
     print()
