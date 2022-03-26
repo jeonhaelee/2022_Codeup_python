0b5ce20a51a892277a4377e23b442e132fac104b
jeonhaelee <jhealee0624@gmail.com>
2022-03-26 11:06:47 +0900

    2차원 배열 지그재그 채우기 2-8

---
+++ codeup_1475.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+n, m = map(int,input().split())
+
+d = np.arange(n*m).reshape(n,m)
+
+num = 1
+if m % 2 == 0:
+    for j in range(m):
+        if j % 2 != 0:
+            for i in range(n):
+                d[n-1-i,m-1-j] = num
+                num += 1
+        else :
+            for i in range(n):
+                d[i,m-1-j] = num
+                num += 1
+else :
+    for j in range(m):
+        if j % 2 == 0:
+            for i in range(n):
+                d[i,m-1-j] = num
+                num += 1
+        else :
+            for i in range(n):
+                d[n-1-i,m-1-j] = num
+                num += 1
+        
+for i in range(n):
+    for j in range(m):
+        print(d[i,j], end = " ")
+    print()
