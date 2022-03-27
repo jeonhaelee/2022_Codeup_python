28b3722097c19feea3910afcd7dbf2f597de3138
jeonhaelee <jhealee0624@gmail.com>
2022-03-27 23:20:00 +0900

    버블정렬_fin

--- codeup_1441.py
+++ codeup_1441.py
@@ -2,13 +2,14 @@ n = int(input())
 d = []
 for i in range(n):
     d.append(int(input()))
-    
-for i in range(n):
-    for j in range(n-1):
+
+for j in range(n):
+    for i in range(n-1):
         if d[i] > d[i+1]:
             temp = d[i]
             d[i] = d[i+1]
             d[i+1] = temp
-            
+     
+
 for i in d:
     print(i)
