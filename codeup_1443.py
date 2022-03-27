bd803667a9eecd3431afc077bb75ff286d5789e4
jeonhaelee <jhealee0624@gmail.com>
2022-03-28 01:19:23 +0900

    삽입장렬_fin

--- codeup_1443.py
+++ codeup_1443.py
@@ -6,14 +6,11 @@ for i in range(n):
     d.append(int(input()))
 
 for i in range(1,n):
-    for j in range(i):
-        if d[i] >= d[j]:
-            continue
-        else :
-            temp = d[i]
-            d[i] = d[j-1]
-            d[j-1] = temp
-            
+    for j in range(i,0,-1):
+        if d[j-1] > d[j]:
+            temp = d[j-1]
+            d[j-1] = d[j]
+            d[j] = temp
+        
 for i in d:
     print(i)
-    
