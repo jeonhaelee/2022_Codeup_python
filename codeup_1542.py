a78567483e338dc11b5e99f4ebb24f434aa9d1b9
jeonhaelee <jhealee0624@gmail.com>
2022-03-29 21:43:35 +0900

    함수로 prime 또는 composite 출력하기

---
+++ codeup_1542.py
@@ -0,0 +1,13 @@
+
+
+def f(n):
+    count = 0
+    for i in range(1,n+1):
+        if n % i == 0:
+            count += 1
+    if count == 2:
+        return print("prime")
+    else:
+        return print("composite")
+
+f(int(input()))
