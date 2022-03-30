f73ddf569401fc08c85fbe1a9dcdd5275a0aba4d
jeonhaelee <jhealee0624@gmail.com>
2022-03-30 22:51:33 +0900

    정렬을 빠르게_계수정렬_nonfin

--- codeup_3014.py
+++ codeup_3014.py
@@ -10,18 +10,18 @@ import numpy as np
 n = int(input())
 d = np.array(input().split(), dtype=int)
 
-c = [0] * 100001
+count = np.zeros(100001,dtype=int)
 answer = [0] * n
-    
+
 for i in d:
-    c[i] += 1
+    count[i] += 1
     
 for i in range(1,100001):
-    c[i] += c[i-1]
+    count[i] += count[i-1]
    
 for i in range(n):
-    answer[c[d[i]]-1] = d[i]
-    c[d[i]] -= 1
+    answer[count[d[i]]-1] = d[i]
+    count[d[i]] -= 1
 
 for i in answer:
     print(i, end = " ")
