4e4bc019e600db1937c5de865146b8fed2f77006
jeonhaelee <jhealee0624@gmail.com>
2022-03-30 18:21:57 +0900

    기억력 테스트 2 4

--- codeup_1430.py
+++ codeup_1430.py
@@ -1,23 +1,37 @@
 
+# import sys
+# import numpy as np
+
+# n = int(sys.stdin.readline().rstrip())
+# d1 = sys.stdin.readline().rstrip()
+# origin = np.array(d1.split())
+
+# m = int(sys.stdin.readline().rstrip())
+# d2 = sys.stdin.readline().rstrip()
+# test = np.array(d2.split())
+
+# for i in test:
+#     if i in origin:
+#         print(1, end = " ")
+#     else : print(0, end = " ")
+
+
 import sys
 import numpy as np
 
 n = int(sys.stdin.readline().rstrip())
-d1 = sys.stdin.readline().rstrip()
-origin = np.array(d1.split())
+d1 = np.zeros(10000000, dtype=int)
+d1 = sys.stdin.readline().rstrip().split()
 
 m = int(sys.stdin.readline().rstrip())
-d2 = sys.stdin.readline().rstrip()
-test = np.array(d2.split())
+d2 = np.arange(100000, dtype=int)
+d2 = sys.stdin.readline().rstrip().split()
 
-for i in test:
-    if i in origin:
+for i in d2:
+    if i in d1:
         print(1, end = " ")
     else : print(0, end = " ")
 
-
-
-   
 
 
         
