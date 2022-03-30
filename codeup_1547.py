1d4c9b121648a3e474cb54860e462804cac26e8c
jeonhaelee <jhealee0624@gmail.com>
2022-03-30 16:15:34 +0900

    함수로 prime/composite 판별하기

---
+++ codeup_1547.py
@@ -0,0 +1,14 @@
+
+
+
+def prime(n):
+    count = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            count += 1
+    if count == 2:
+        print("prime")
+    else: print("composite")
+    
+num = int(input())
+prime(num)
