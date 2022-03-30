7f4f624c18ce9ea37fd52d618d949dcb93b4b4e6
jeonhaelee <jhealee0624@gmail.com>
2022-03-30 17:34:12 +0900

    기억력 테스트 2 2

--- codeup_1430.py
+++ codeup_1430.py
@@ -1,10 +1,25 @@
+
 n = int(input())
 d = list(map(int,input().split()))
 
 m = int(input())
-q = list(map(int,input().split()))
+q = input()
 
-for i in range(m):
-    if q[i] in d:
-        print(1, end = " ")
-    else : print(0, end = " ")
+num = ""
+for i in range(len(q)):
+    if q[i] == " ":
+        if int(num) in d:
+            print(1, end = " ")
+        else : print(0, end = " ")
+        num = ""
+        continue
+    num += q[i]
+if int(num) in d:
+    print(1, end = " ")
+else : print(0, end = " ")
+    
+
+   
+
+
+        
