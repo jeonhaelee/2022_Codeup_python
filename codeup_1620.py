8835db097027ee501c90ec223d4a09bc65511ed2
jeonhaelee <jhealee0624@gmail.com>
2022-04-06 00:32:22 +0900

    자릿수의 합_fin

--- codeup_1620.py
+++ codeup_1620.py
@@ -5,10 +5,11 @@ def str_sum(str_num):
     sum = 0
     sum = np.array(list(map(lambda x : int(str_num[x]), range(len(str_num)))))
     return np.sum(sum)
-
+    
 def sum_one(num):
-    while num != 1:
+    while num // 10 != 0:
         num = str_sum(str(num))
-    return print(1)
+    return print(num)
 
 sum_one(int(input()))
+
