d5cb7d757bce47c03abd564cf9add1f38eaba3cd
jeonhaelee <jhealee0624@gmail.com>
2022-04-09 13:28:53 +0900

    기억력 테스트 9 4

--- codeup_3170.py
+++ codeup_3170.py
@@ -3,10 +3,17 @@ import sys
 
 n, m = map(int, input().split())
 
+dics = {}
 for i in range(n):
+    word, number = sys.stdin.readline().rstrip().split()
+    number = int(number)
+    if word in dics.keys():
+        dics[word] += number
+    else : dics[word] = number
     
 for i in range(m):
     quiz = sys.stdin.readline().rstrip()
+    print(dics[quiz])
 
 
 
