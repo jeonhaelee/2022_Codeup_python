b78839b53f84fefd277a089b20630247b8921be9
jeonhaelee <jhealee0624@gmail.com>
2022-04-09 12:31:05 +0900

    기억력 테스트 9 2

--- codeup_3170.py
+++ codeup_3170.py
@@ -1,4 +1,6 @@
 
+import sys
+
 def get_sum (quiz, dics):
     sum = 0
     for i in range(len(words)):
@@ -11,16 +13,18 @@ words = []
 dics = []
 
 for i in range(n):
-    word, number = input().split()
+    word, number = sys.stdin.readline().rstrip().split()
     words.append(word)
     dics.append((word,int(number)))
     
 for i in range(m):
-    quiz = input()
-    if quiz not in words:
+    quiz = sys.stdin.readline().rstrip()
+    if words.count(quiz) == 0:
         print(0)
     else:
         if words.count(quiz) >= 2:
             print(get_sum(quiz, dics))
         else:
             print(dics[words.index(quiz)][1])
+
+
