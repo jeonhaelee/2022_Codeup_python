ea56abdeca074286b6b4f1fd3d1f853918e9cb4e
jeonhaelee <jhealee0624@gmail.com>
2022-04-13 21:51:55 +0900

    2진수 변환

--- codeup_1920.py
+++ codeup_1920.py
@@ -6,11 +6,11 @@ def f(n, sub_answer):
         sub_answer += str(n % 2)
         f(n//2, sub_answer)
     else:
-        answer = ""
-        for i in range(len(sub_answer)-1,-1,-1):
-            answer += str(sub_answer[i])
-        return print(answer)
+        return print(sub_answer[::-1])
 
 sub_answer = ""
 n = int(input())
-f(n, sub_answer)
+
+if n == 0:
+    print(0)
+else : f(n, sub_answer)
