e33b28bf13b2e6b3118261488a451be7ec098284
jeonhaelee <jhealee0624@gmail.com>
2022-04-15 00:15:03 +0900

    천단위 구분기호

--- codeup_2016.py
+++ codeup_2016.py
@@ -1,10 +1,13 @@
 
 n = int(input())
 number = input()
+
+
 num = number[::-1]
 answer = ""
 for i in range(n):
     answer += num[i]
-    if (i+1) % 3 == 0:
-        answer += ','
+    if i != n-1:
+        if (i+1) % 3 == 0:
+            answer += ','
 print(answer[::-1])
