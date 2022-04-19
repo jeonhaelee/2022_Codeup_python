a576b3c56147cf54227aba7cf739002db9c44e5d
jeonhaelee <jhealee0624@gmail.com>
2022-04-20 00:17:15 +0900

    2차원 배열 채우기 3(달팽이 배열)_nonfin

--- codeup_1505.py
+++ codeup_1505.py
@@ -7,30 +7,43 @@ d = np.zeros(n*n, dtype=np.int64).reshape(n,n)
 
 row = 0; col = 0
 count = 1
+for j in range(n):
+    d[row, j] = count
+    count += 1
+row += 1
+col = j
 while 0 in d:
-    for j in range(n):
-        d[row, j] = count
-        count += 1
-        print(d)
-    row += 1
-    col = j
     for i in range(row, n):
+        if d[i, col] != 0:
+            i -= 1
+            break
         d[i, col] = count
         count += 1
-        print(d)
     row = i
     col -= 1
-    for j in range(col-1,-1,-1):
+    for j in range(col,-1,-1):
+        if d[row, j] != 0:
+            j += 1
+            break
         d[row, j] = count
         count += 1
-        print(d)
     row -= 1
     col = j
-    for i in range(row-1,-1,-1):
+    for i in range(row,-1,-1):
+        if d[i, col] != 0:
+            i += 1
+            break
         d[i, col] = count
         count += 1
-        print(d)
     row = i
     col += 1
-    
+    for j in range(col, n):
+        if d[row, j] != 0:
+            j -= 1
+            break
+        d[row, j] = count
+        count += 1
+
+
+
 print(d)
