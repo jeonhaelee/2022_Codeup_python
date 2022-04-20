d5c3c6d36460f144c8528b94e9bf855f96facd16
jeonhaelee <jhealee0624@gmail.com>
2022-04-21 00:02:34 +0900

    두 용액_nonfin

--- codeup_4701.py
+++ codeup_4701.py
@@ -20,8 +20,8 @@ print(answer)
 min = answer[0][0]
 min_idx = 0
 for i in range(1, len(answer)):
-    if (answer[i][0] < min).all():
-        min = answer[i]
+    if answer[i][0] < min:
+        min = answer[i][0]
         min_idx = i
         
 if answer[min_idx][1] > answer[min_idx][2]:
