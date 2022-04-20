c81f597793b602d3bf0d72bcc8dffffebdfcf2de
jeonhaelee <jhealee0624@gmail.com>
2022-04-20 23:18:27 +0900

    두 용액_nonfin

--- codeup_4701.py
+++ codeup_4701.py
@@ -1,22 +1,31 @@
 
+
 import numpy as np
+from itertools import combinations
+
 n = int(input())
 d = np.array(input().split(), dtype=np.int64)
-
-zero = abs(d[0]+d[1])
-index_i = 0
-index_j = 1
-for i in range(n-1):
-    for j in range(i+1, n):
-        test = abs(d[i]+d[j])
-        if test < zero:
-            zero = test
-            index_i = i
-            index_j = j
-
-if d[index_i] > d[index_i]:
-    print(d[index_j], end = " ")
-    print(d[index_i])
+result = list(combinations(d,2))
+answer = []
+for i in range(len(result)):
+    answer.append((abs(result[i][0]+result[i][1]), result[i][0], result[i][1]))
+answer.sort(key=lambda x:x[0])
+if answer[0][1] > answer[0][2]:
+    print(answer[0][2], end=" ")
+    print(answer[0][1])
 else:
-    print(d[index_i], end = " ")
-    print(d[index_j])
+    print(answer[0][1], end=" ")
+    print(answer[0][2])
+
+# d = [1, 2, 3]
+# result = list(combinations(d,2))
+# answer = []
+# for i in range(len(result)):
+#     answer.append((result[i][0]+result[i][1], result[i][0], result[i][1]))
+# answer.sort(key=lambda x:x[0])
+# if answer[0][1] > answer[0][2]:
+#     print(answer[0][2], end=" ")
+#     print(answer[0][1])
+# else:
+#     print(answer[0][1], end=" ")
+#     print(answer[0][2])
