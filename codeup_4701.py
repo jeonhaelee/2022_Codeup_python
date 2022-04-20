25e1988168070610c0f517041792790988e56a5a
jeonhaelee <jhealee0624@gmail.com>
2022-04-20 23:56:57 +0900

    두 용액_nonfin

--- codeup_4701.py
+++ codeup_4701.py
@@ -5,27 +5,28 @@ from itertools import combinations
 
 n = int(input())
 d = np.array(input().split(), dtype=np.int64)
-result = list(combinations(d,2))
-answer = []
-for i in range(len(result)):
-    answer.append((abs(result[i][0]+result[i][1]), result[i][0], result[i][1]))
-answer.sort(key=lambda x:x[0])
-if answer[0][1] > answer[0][2]:
-    print(answer[0][2], end=" ")
-    print(answer[0][1])
-else:
-    print(answer[0][1], end=" ")
-    print(answer[0][2])
 
-# d = [1, 2, 3]
-# result = list(combinations(d,2))
-# answer = []
-# for i in range(len(result)):
-#     answer.append((result[i][0]+result[i][1], result[i][0], result[i][1]))
-# answer.sort(key=lambda x:x[0])
-# if answer[0][1] > answer[0][2]:
-#     print(answer[0][2], end=" ")
-#     print(answer[0][1])
-# else:
-#     print(answer[0][1], end=" ")
-#     print(answer[0][2])
+result = np.array(list(combinations(d,2)))
+print(result)
+answer = np.zeros(len(result)*3, dtype=np.int64).reshape(len(result),3)
+
+for i in range(len(result)):
+    answer[i][0] = abs(result[i][0]+result[i][1])
+    answer[i][1] = result[i][0]
+    answer[i][2] = result[i][1]
+
+print(answer)
+
+min = answer[0][0]
+min_idx = 0
+for i in range(1, len(answer)):
+    if (answer[i][0] < min).all():
+        min = answer[i]
+        min_idx = i
+        
+if answer[min_idx][1] > answer[min_idx][2]:
+    print(answer[min_idx][2], end=" ")
+    print(answer[min_idx][1])
+else:
+    print(answer[min_idx][1], end=" ")
+    print(answer[min_idx][2])
