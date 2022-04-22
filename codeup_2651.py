48bce34ae8de5edc18ba13f086d5c5491cf92ad6
jeonhaelee <jhealee0624@gmail.com>
2022-04-22 23:41:50 +0900

    극장 좌석 배치 1

--- codeup_2651.py
+++ codeup_2651.py
@@ -1,10 +1,13 @@
 
+
+def f(a, b):
+    answer = 1
+    for i in range(b):
+        answer *= (a - i)
+    return answer
+    
 import math
 a, b = map(int,input().split())
-if b == 1:
-    answer = a
-elif math.factorial(a)/math.factorial(b) == 1:
-    answer = 1
-else:
-    answer = math.factorial(a)/math.factorial(b)/2
-print(int(answer))
+answer = f(a, b)
+
+print(int(answer/2))
