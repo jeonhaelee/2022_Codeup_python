f9acfa2b766d12923eb23001f48dd3c22fdf5e12
jeonhaelee <jhealee0624@gmail.com>
2022-04-23 14:08:06 +0900

    극장 좌석 배치 2 _nonfin

--- codeup_2652.py
+++ codeup_2652.py
@@ -1,19 +1,27 @@
 
 import math
+import numpy as np
 
 n, k = map(int,input().split())
+seats = np.zeros(n, dtype=np.int64)
+print(seats)
 
 def f(n, k):
-    answer = 1
+    sum = 1
+    count = 0
+    for i in range(n):
+        if seats[i] == 0:
+            count += 1 
+    sum *= count
     if k == 1:
-        answer = n
-        return answer
-    elif n == k:
-        return answer
-    else:
-        for i in range(k):
-            answer *= (n - i)
-        return answer/math.factorial(k)
-    
-result1 = int(f(n, k))
+        return sum
+    else : f(n-3, k-1)
 
+result = 1
+if k == 1:
+    result = n
+else : 
+    result = f(n,k)
+    result = int(result/2)
+
+print(result)
