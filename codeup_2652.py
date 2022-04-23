19d176318502a5cc4f754dda08f4daf99b10d309
jeonhaelee <jhealee0624@gmail.com>
2022-04-23 13:52:06 +0900

    극장 좌석 배치 2_nonfin

---
+++ codeup_2652.py
@@ -0,0 +1,19 @@
+
+import math
+
+n, k = map(int,input().split())
+
+def f(n, k):
+    answer = 1
+    if k == 1:
+        answer = n
+        return answer
+    elif n == k:
+        return answer
+    else:
+        for i in range(k):
+            answer *= (n - i)
+        return answer/math.factorial(k)
+    
+result1 = int(f(n, k))
+
